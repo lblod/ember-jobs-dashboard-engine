fc8cc2724dfe389f893cca97a5a9385437447d1f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-04-30 17:54:45 +0200

    Use a more specific includePath
    With the regular `node_modules` include path, the app takes a long time to build. Switching to a more specific one solves that (but requires us to change the import as well..).

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
     sassOptions: {
-      includePaths: ['node_modules'],
+      includePaths: ['node_modules/@appuniversum/'],
     },
     '@appuniversum/ember-appuniversum': {
       disableWormholeElement: true,
